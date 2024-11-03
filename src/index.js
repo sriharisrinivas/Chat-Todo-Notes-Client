36bfa3badc921a904e484a7533b18b79b95a59e5
SRIHARI SRINIVAS <sriharisrinivas995@gmail.com>
2024-11-03 17:49:36 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
-import AppV1 from './AppV1';
+// import AppV1 from './AppV1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
